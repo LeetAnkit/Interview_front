98ddc4144aef998cf0b6ab1cf844b0e4f7db2746
LeetAnkit <134221882+LeetAnkit@users.noreply.github.com>
2025-09-07 21:26:06 +0530

    latest commit

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Downloading Flutter SDK..."
-git clone https://github.com/flutter/flutter.git -b stable
+if [ ! -d "flutter" ]; then
+  echo "Downloading Flutter SDK..."
+  git clone https://github.com/flutter/flutter.git -b stable
+else
+  echo "Flutter SDK already exists. Skipping download."
+fi
+
 export PATH="$PATH:$PWD/flutter/bin"
 
 echo "Flutter version:"
 flutter --version
+
+echo "Running flutter doctor..."
+flutter doctor
+
+echo "Enabling web support..."
+flutter config --enable-web
+flutter precache
