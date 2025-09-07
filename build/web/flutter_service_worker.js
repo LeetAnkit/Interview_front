5ebdf857e1aaff42b8a9689b9962c9e6fa1e0120
LeetAnkit <134221882+LeetAnkit@users.noreply.github.com>
2025-09-07 22:10:12 +0530

    fedwf

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b57f94c28cf7f97b819b8881597ea19
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ac6299e5a81ae400bca3a151fff4697e",
+"flutter_bootstrap.js": "13ecf4784c7f1c11f83856baabf96342",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
